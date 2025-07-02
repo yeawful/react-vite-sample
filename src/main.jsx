e6410ddd5332af6567ec79254d2528268982bcd9
Ilya Raketa <paketa1337@gmail.com>
2025-07-02 16:36:22 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App/App.jsx';
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./components/App/App.jsx";
+import './styles/index.scss'
+import '@a1rth/css-normalize'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
     </StrictMode>,
